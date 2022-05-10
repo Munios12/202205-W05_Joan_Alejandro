53ca96c6fa2626bd70a60d1dea423b21af5f4b36
joanmontiel4 <joanmontiel@gmail.com>
2022-05-10 12:37:41 +0200

    Improve join.js

--- join/join.js
+++ join/join.js
@@ -1,17 +1,17 @@
-export function join(array, separator) {
-    const joinElements = (array, separator) => {
-        let joinString = '';
-        for (let i = 0; i < array.length; i++) {
-            if (i === array.length - 1) {
-                separator = '';
-            }
-            joinString += array[i] + separator;
-        }
-        return joinString;
-    };
-
-    if (separator === '' || separator === null || separator === undefined) {
-        return joinElements(array, '');
+export function join(array = [], separator = '') {
+    if (separator === null || separator === undefined) {
+        separator = '';
     }
-    return joinElements(array, separator);
+    let joinString = '';
+    for (let i = 0; i < array.length; i++) {
+        if (i < array.length - 1) {
+            joinString += array[i] + separator;
+        } else {
+            joinString += array[i];
+        }
+    }
+    return joinString;
 }
+
+const test = join(['apple', 2.3], ', ');
+console.log(test);
