252e0288e2edb4b967465c4828c79f4325f7af7e
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2022-05-11 15:25:36 +0200

    add falseUnshift

--- script/functionWithVeggies.js
+++ script/functionWithVeggies.js
@@ -5,3 +5,29 @@ export const falsePush = (arr, item) => {
     myArray[myArray.length] = item;
     return myArray;
 };
+
+export const falsePop = (arr) => {
+    let myArr = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        myArr = falsePush(myArr, arr[i]);
+    }
+
+    return myArr;
+};
+
+export const falseShift = (arr) => {
+    let myArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        myArr = falsePush(myArr, arr[i]);
+    }
+
+    return myArr;
+};
+
+export const falseUnshift = (arr, item) => {
+    let myArr = [item, ...arr];
+
+    return myArr;
+};
