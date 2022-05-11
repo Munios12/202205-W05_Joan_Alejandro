1e1bf55d92d2cee46e8c443633ce4040be912cf9
joanmontiel4 <joanmontiel@gmail.com>
2022-05-11 17:57:00 +0200

    Fixing bugs reduce

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -1,11 +1,11 @@
-export function reduce(callback, array, initialValue) {
+export function reduce(func, array, initialValue) {
     const myTypeError = new TypeError();
-    if (typeof callback !== 'function') {
-        myTypeError.messageUX = 'callback must be a function';
+    if (typeof func !== 'function') {
+        myTypeError.message = 'func must be a function';
         throw myTypeError;
     }
     if (!Array.isArray(array)) {
-        myTypeError.messageUX = 'Cannot reduce an undefined array';
+        myTypeError.message = 'Cannot reduce an undefined array';
         throw myTypeError;
     }
     let numberOfValues = 0;
@@ -17,7 +17,7 @@ export function reduce(callback, array, initialValue) {
         }
     }
     if (numberOfValues === 0 && !initialValue) {
-        myTypeError.messageUX =
+        myTypeError.message =
             'Cannot reduce an empty array without an initial value';
         throw myTypeError;
     } else if (numberOfValues === 0 && initialValue) {
@@ -30,43 +30,17 @@ export function reduce(callback, array, initialValue) {
         let previousValue = array[0];
         let actualValue = array[1];
         for (let i = 1; i < array.length - 1; i++) {
-            previousValue = callback(previousValue, actualValue);
+            previousValue = func(previousValue, actualValue);
             actualValue = array[i + 1];
         }
-        return callback(previousValue, actualValue);
+        return func(previousValue, actualValue);
     } else {
         let previousValue = initialValue;
         let actualValue = array[0];
         for (let i = 0; i < array.length - 1; i++) {
-            previousValue = callback(previousValue, actualValue);
+            previousValue = func(previousValue, actualValue);
             actualValue = array[i + 1];
         }
-        return callback(previousValue, actualValue);
+        return func(previousValue, actualValue);
     }
 }
-
-const callback = (a, b) => a + b;
-const testArray = [0, 1, 2, 3];
-console.log(testArray);
-// console.log(typeof testArray);
-// const initialV = null;
-console.log('Results:');
-const testResult = reduce(callback, testArray);
-console.log(typeof testResult);
-console.log(testResult);
-
-// console.log(typeof testArray);
-const initialV = 10;
-console.log('Results:');
-const testResult2 = reduce(callback, testArray, initialV);
-console.log(typeof testResult2);
-console.log(testResult2);
-
-const testArray2 = [0];
-console.log(testArray2);
-// console.log(typeof testArray);
-// const initialV = null;
-console.log('Results:');
-const testResult3 = reduce(callback, testArray2);
-console.log(typeof testResult3);
-console.log(testResult3);
--- reduce/reduce.test.js
+++ reduce/reduce.test.js
@@ -4,11 +4,11 @@ describe('Given the function reduce', () => {
     describe('When receive (a,b)=>a+b, [0, 1, 2, 3]', () => {
         test('Then should be 6', () => {
             // Arrange
-            const callback = (a, b) => a + b;
+            const func = (a, b) => a + b;
             const testArray = [0, 1, 2, 3];
             const initial = 6;
             // Act
-            const result = reduce(callback, testArray);
+            const result = reduce(func, testArray);
             // Assert
             expect(result).toBe(initial);
         });
@@ -16,7 +16,7 @@ describe('Given the function reduce', () => {
     describe('When receive (a,b)=>a.concat(b), [[0,1],[2,3],[4,5]]', () => {
         test('Then should be [0,1,2,3,4,5]', () => {
             // Arrange
-            const callback = (a, b) => a.concat(b);
+            const func = (a, b) => a.concat(b);
             const testArray = [
                 [0, 1],
                 [2, 3],
@@ -24,7 +24,7 @@ describe('Given the function reduce', () => {
             ];
             const initial = [0, 1, 2, 3, 4, 5];
             // Act
-            const result = reduce(callback, testArray);
+            const result = reduce(func, testArray);
             // Assert
             expect(result).toStrictEqual(initial);
         });
@@ -32,24 +32,74 @@ describe('Given the function reduce', () => {
     describe('When receive (a,b)=>a+b, [5]', () => {
         test('Then should be 5', () => {
             // Arrange
-            const callback = (a, b) => a.concat(b);
+            const func = (a, b) => a.concat(b);
             const testArray = [5];
             const initial = 5;
             // Act
-            const result = reduce(callback, testArray);
+            const result = reduce(func, testArray);
             // Assert
             expect(result).toBe(initial);
         });
     });
-    describe('When receive (a,b)=>a+b, [], 4', () => {
-        test('Then should be 4', () => {
+    describe('When receive (a,b)=>a+b, [], 5', () => {
+        test('Then should be 5', () => {
             // Arrange
-            const callback = (a, b) => a.concat(b);
+            const func = (a, b) => a.concat(b);
+            const testArray = [];
+            const initialV = 5;
+            const initial = 5;
+            // Act
+            const result = reduce(func, testArray, initialV);
+            // Assert
+            expect(result).toBe(initial);
+        });
+    });
+    describe('When receive (a,b)=>a+b, [], 5', () => {
+        test('Then should be 5', () => {
+            // Arrange
+            const func = (a, b) => a + b;
+            const testArray = [0, 1, 2, 3, 4];
+            const initialV = 5;
+            const initial = 15;
+            // Act
+            const result = reduce(func, testArray, initialV);
+            // Assert
+            expect(result).toBe(initial);
+        });
+    });
+    describe('When receive an undefined array an initial value', () => {
+        test('Then should throw an error', () => {
+            // Arrange
+            const func = (a, b) => a.concat(b);
             const testArray = undefined;
             const initialValue = 4;
             // Act & Assert
-            expect(() => reduce(callback, testArray, initialValue)).toThrow(
-                TypeError('Array is not defined')
+            expect(() => reduce(func, testArray, initialValue)).toThrow(
+                TypeError('Cannot reduce an undefined array')
+            );
+        });
+    });
+    describe('When receive empty array and undefined initial value', () => {
+        test('Then should throw an error', () => {
+            // Arrange
+            const func = (a, b) => a.concat(b);
+            const testArray = [];
+            // Act & Assert
+            expect(() => reduce(func, testArray)).toThrow(
+                TypeError(
+                    'Cannot reduce an empty array without an initial value'
+                )
+            );
+        });
+    });
+    describe('When do not receive a func function', () => {
+        test('Then should throw an error', () => {
+            // Arrange
+            const func = null;
+            const testArray = [0, 1, 2];
+            // Act & Assert
+            expect(() => reduce(func, testArray)).toThrow(
+                TypeError('func must be a function')
             );
         });
     });
